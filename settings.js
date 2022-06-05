6d322365337b9a3f0e713083d82e0032a7c1db0e
Kumuthu <kumuthuprabhasha@gmail.com>
2022-06-05 17:20:03 +0530

    🤭RED DRAGON BOT🌹

---
+++ settings.js
@@ -0,0 +1,74 @@
+//════════════(00)═══════════════════════════════════════//
+//
+//                              K.PRABHASHA
+//𝙰𝙳𝙾𝙿𝚃𝙴𝙳 𝙵𝚁𝙾𝙼  𝚂𝙲𝚁𝙸𝙿𝚃 𝙾𝙵 𝙲𝙷𝙴𝙴𝙼𝚂𝙱𝙾𝚃 𝚅2 𝙱𝚈 𝙳𝙶𝚇𝚎𝚘𝚗 
+//
+//════════════(00)════════════════//
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+}
+
+//customize settings\\
+global.owner = ['380944814219'] //DO NOT CHANGE!
+global.premium = ['380944814219'] //DO NOT CHANGE!
+global.ownernomer = '380944814219' //DO NOT CHANGE!
+global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //YOU CAN CHANGE<<<
+global.botname = ' ☬RED༒DRAGON࿐' //YOU CAN CHANGE<<<
+/*global.footer = '©_Kumuthu.'*/
+global.ig = 'https://github.com/nexusNw'
+global.region = 'India, South India, Kerala'
+global.sc = 'https://sites.google.com/view/queenelsabot/home' //Do not change.!
+global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ' //===
+global.packname = '☬RED༒DRAGON࿐' //YOU CAN CHANGE<<<
+global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯'
+global.sessionName = 'session'
+global.prefa = ['','!','.','😂','❤','💠']
+global.sp = '⭔'
+global.footer = '©_Kumuthu.'
+global.mess = {
+    success: 'Done ✅',
+    admin: 'Only Admins can use it!😉',
+    botAdmin: 'Bot wants to be an Admin😥',
+    owner: 'Only Owner can use this!😉',
+    group: 'Feature Used Only For Groups!',
+    private: 'This Features Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot😝',
+    wait: '👉👈Pleas Wait...',
+    error: 'Error!!! Maybe Api Key Is Expired🤔!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./DragonMedia/image/red-_-dragon-kumuthu.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
