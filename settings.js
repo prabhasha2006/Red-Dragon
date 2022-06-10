e5c371aa4e649fb850815ea2960a01961555777f
Kumuthu <102839813+prabhasha2006@users.noreply.github.com>
2022-06-10 19:24:33 +0530

    🤭RED DRAGON BOT🌹

--- settings.js
+++ settings.js
@@ -30,8 +30,8 @@ global.ownernomer = '380944814219' //Owner Number 3
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
 
-global.button = '📱YouTube📍' //Costomize A Button Name In Alive Message
-global.btnurl = '' //Change Your Button's URL
+global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
+// global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
