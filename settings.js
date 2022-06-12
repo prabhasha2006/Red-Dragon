ab62c87ca764bfd21a69f26504ee6cd2f9cee275
Kumuthu <102839813+prabhasha2006@users.noreply.github.com>
2022-06-12 21:57:41 +0530

    🤭RED DRAGON BOT🌹

--- settings.js
+++ settings.js
@@ -23,9 +23,9 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number 1
-global.premium = ['+380944814219'] //Owner Number 2
-global.ownernomer = '+380944814219' //Owner Number 3
+global.owner = ['+380944814219'] //Owner number in aive msg
+global.premium = ['+380944814219'] //Owner Number info
+global.ownernomer = '+380944814219' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON' //Bot Name
