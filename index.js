c694e4cd4ee754ad5d976de36693ed6c64df66b5
Sjshskdhd <108340300+Sjshskdhd@users.noreply.github.com>
2022-06-28 13:57:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -145,16 +145,16 @@ Kon = await getBuffer(`https://hardianto.xyz/api/welcome3?profile=${encodeURICom
 Tol = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURIComponent(ppuser)}&name=${encodeURIComponent(nama)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`)
                 if (anu.action == 'add') {
                     RedDragonMdNx.sendMessage(anu.id, { image: Kon, contextInfo: { mentionedJid: [num] }, caption: `
-⭐✑ Hi👋 @${num.split("@")[0]},
-⭐✑ Welcome To ${metadata.subject}
+⭐✑ اهلا👋 @${num.split("@")[0]},
+⭐✑ مرحبا بك ${metadata.subject}
 
-⭐✑ Description: ${metadata.desc}
+⭐✑ الوصف: ${metadata.desc}
 
-⭐✑ Welcome To Our Comfortable Happy😋, Sometimes Loud😜, Usually Messy🤥, Full Of Love🥰, HOME😌!!`} )
+⭐✑ نورت الجروب يقمر❤,يجب احترام الادمن, والالتزام بالقوانين في الوصف!!`} )
                 } else if (anu.action == 'remove') {
                     RedDragonMdNx.sendMessage(anu.id, { image: Tol, contextInfo: { mentionedJid: [num] }, caption: `⭐✑ @${num.split("@")[0]} Left ${metadata.subject}
 
-⭐✑ I'm Not Sure If It Was A Goodbye Charm, But It Was Fun While It Lasted 😌✨` })
+⭐✑ حد يخرج من جروب قمر زي دا دا حتا كلنا اخوات واصحاب❤،،` })
                 }
             }
         } catch (err) {
