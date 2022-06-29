936adad5ddc18b6217151656d4c4f3e07ce10e24
Oww Shadow <107193785+Owwshadow@users.noreply.github.com>
2022-06-29 22:19:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 //════════════════════════════//
 
 require('./settings')
-require('./DragonSpam')
 const { default: NexusNwIncConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
