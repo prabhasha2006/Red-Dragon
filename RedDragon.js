04a867c89980579e5559678097e54a3cdaf332c0
Oww Shadow <107193785+Owwshadow@users.noreply.github.com>
2022-07-03 21:25:11 +0530

    3832

--- RedDragon.js
+++ RedDragon.js
@@ -3825,12 +3825,19 @@ let template = await generateWAMessageFromContent(m.chat, proto.Message.fromObje
                     image: { url: 'https://i.ibb.co/FKJvwZ3/20220703-093716.jpg'},
                 caption: `*Your Version is 6.0*\n\ncheck your updates\nhttps://sites.google.com/view/red-dragon-bot/home` ,
                 footer: '◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ʙᴏᴛ MD',
-                react: "✅",
+                ✅
                 buttons,
                 headerType: 4
                 }
-                RedDragonMdNx.sendMessage(m.chat, buttonMessage, { quoted: m })
-            RedDragonMdNx.sendReaction(m.chat, {react: "✅"} { quoted: m })
+                RedDragonMdNx.sendMessage(m.chat, buttonMessage, { quoted: m }
+		reactionMessage = {
+                    react: {
+                        text: '✅',
+                        key: { remoteJid: m.chat, fromMe: true, id: quoted.id }
+                    }
+                }
+                RedDragonMdNx.sendMessage(m.chat, reactionMessage)
+            }
             }
             break    
             case 'allmenu': case 'menuall': {
