2f6608cf0e4a15cf9c82d6a0d6d3aa3e092dd5e4
koshitha1 <108674924+koshitha1@users.noreply.github.com>
2022-07-05 09:34:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -39,7 +39,7 @@ global.author = '😎සුල්තාන් 💞❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
-global.dragonemoji = '🧩' // Menu emoji
+global.dragonemoji = '😘🙈' // Menu emoji
 global.footer = '©_Kumuthu.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
