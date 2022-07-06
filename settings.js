b801c83a03d6464fec557af16aae918348c0e5e9
rashmikagayashan <108579138+rashmikagayashan@users.noreply.github.com>
2022-07-06 10:13:01 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,26 +20,26 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+My Creator is Gayashan😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94742584766'] //Owner number in aive msg
+global.premium = ['+94742584766'] //Owner Number info
+global.ownernomer = '+94742584766' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎official Gaya' //Owner Name
+global.botname = ' ☬Official Gaya' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
 global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'Official Gaya' //Bot Pacage Name
+global.author = '😎Gayashan❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
-global.dragonemoji = '🧩' // Menu emoji
+global.dragonemoji = '🧩🤗 ' // Menu emoji
 global.footer = '©_Kumuthu.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
@@ -54,14 +54,14 @@ global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
-    error: 'Error!!! Maybe Api Key Is Expired🤔!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    wait: '👉👈රැදීසිටින්න...',
+    error: 'Error!!! ඔයාටවැරදිලා 🤔!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 500Hours',
 }
 // DO NOT CHANGE \\
     global.limitawal = {
     premium: "Infinity",
-    free: 12,
+    free: 500,
     monayawal: 1000
 }
    global.rpg = {
