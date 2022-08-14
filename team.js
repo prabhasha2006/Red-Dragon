0d6ef3e62c91d657d4303ee7eabac8d1ecf31bc5
Nora <107586461+noora-bot@users.noreply.github.com>
2022-08-14 14:08:22 +0300

    Update team.js

--- team.js
+++ team.js
@@ -1408,9 +1408,6 @@ case 'halah': case 'hilih': case 'huluh': case 'heleh': case 'holoh': case 'كي
 		if (!m.isGroup) return replay(`${mess.group}`)
                 if (!isBotAdmins) return replay(`${mess.botAdmin}`)        
                 if (!isCreator) return replay(`${mess.owner}`) 
-let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net/nPromoted'
-		await RedDragonMdNx.groupParticipantsUpdate(m.chat, [users], 'promote').then((res) => reply(jsonformat(res))).catch((err) => reply(jsonformat(err)))
-
                 if (!isAdmins) return replay(`${mess.admin}`)  
 		let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net/nPromoted'
 		await RedDragonMdNx.groupParticipantsUpdate(m.chat, [users], 'promote').then((res) => reply(jsonformat(res))).catch((err) => reply(jsonformat(err)))
